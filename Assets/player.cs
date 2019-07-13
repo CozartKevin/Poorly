1d2937a8554526d97a78ef74b2490cef508f2b2c
Raggdoll <Cozartkevin@gmail.com>
2019-07-12 17:34:46 -0700

    Revert "Revert "Working input detection, no movement""
    This reverts commit 6a3238289394ddedf665f3a0122a304b0bd69155.

---
+++ Assets/player.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class player : MonoBehaviour
+{
+    public InputMaster controls;
+    private Rigidbody2D rb2D;
+    
+
+    void Awake()
+    {
+        rb2D = gameObject.AddComponent<Rigidbody2D>();
+        controls = new InputMaster();
+        controls.Player.Shoot.performed += contex => Shoot();
+        controls.Player.Movement.performed += context => Movement(context.ReadValue<Vector2>());
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+      
+    }
+
+    private void Movement(Vector2 direction) {
+        Debug.Log("Move along home"  + direction);
+
+    }
+
+
+
+    void Shoot()
+    {
+        Debug.Log("We shot the sherif!");
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+
+    }
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+
+}
